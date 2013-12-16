fe69c4d0c13aab58006b3a1ebfc9682911daf562
James Liljenquist <jliljenq@gmail.com>
2013-12-16 15:16:38 -0500

    Added a Sprite Component

---
+++ src/Nonbiez/src/com/NonbieSoft/nonbiez/SpriteComponent.java
@@ -0,0 +1,12 @@
+package com.NonbieSoft.nonbiez;
+
+import com.NonbieSoft.engine.IComponent;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class SpriteComponent implements IComponent {
+	public Sprite spr;
+	
+	public SpriteComponent(String fileName) {
+		
+	}
+}
