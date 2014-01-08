76c6b04553a6d509c583a64654ede88eacbccecb
Justin McNamara <jmcnamara7489@gmail.com>
2014-01-08 13:03:15 -0500

    Whatever

---
+++ src/Nonbiez/src/com/NonbieSoft/nonbiez/components/ColiderComponent.java
@@ -0,0 +1,25 @@
+package com.NonbieSoft.nonbiez.components;
+
+import com.NonbieSoft.engine.IComponent;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class ColiderComponent implements IComponent {
+	public Rectangle bounds;
+	
+	
+	// hold rectal data
+	public ColiderComponent(float offsetX, float offsetY, float width, float height){
+		bounds = new Rectangle(offsetX, offsetY, width, height);
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
